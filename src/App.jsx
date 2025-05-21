ac963039e77d3ca106f6cc08f84a4b2425411e7d
Daniel <heritagedaniel03@gmail.com>
2025-05-21 09:58:01 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import './index.css'
+// import './index.css'
+import Navbar from './Components/navbar'
 
 
 const App = () => {
   return (
-    <div className>
-      Hello
+    <div>
+       <Navbar />
     </div>
   )
 }
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Logo from '../assets/Fixitlogo.png'
+// import './index.css'
 
 const navbar = () => {
   return (
     <div>
-        
+        <navbar>
+            <div>
+              <img src={Logo} alt="logo"/>
+            </div>
+            <div>
+              <ul>
+                <li className="text-[50px]">Home</li>
+              </ul>
+            </div>
+        </navbar>
     </div>
   )
 }
